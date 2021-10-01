fa0bc03272fd9dccb5c39d604cb348cd78e5f96a
HoangDuy1999 <tranhoangduy.911@gmail.com>
2021-10-01 20:40:22 +0700

    update api product

--- models/product.model.js
+++ models/product.model.js
@@ -1,17 +1,32 @@
 const db = require('../utils/db');
 module.exports = {
-  findAll() {
-    return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
-      'c.name as category_name', 'c.alias as category_alias',
-      'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
-      'acc2.full_name as bidder_name')
-      .from('product as p')
-      .leftJoin('auction as a', 'a.product_id', 'p.product_id')
-      .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
-      .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
-      .leftJoin('type as t', 't.type_id', 'p.type_id')
-      .leftJoin('category as c', 'c.category_id', 'p.category_id')
-      .where('p.status', '1')
+  findAll(condition_end_day = false) {
+    if (condition_end_day) {
+      return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+        'c.name as category_name', 'c.alias as category_alias',
+        'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+        'acc2.full_name as bidder_name')
+        .from('product as p')
+        .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+        .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+        .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+        .leftJoin('type as t', 't.type_id', 'p.type_id')
+        .leftJoin('category as c', 'c.category_id', 'p.category_id')
+        .where('p.status', '1')
+    } else {
+      return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+        'c.name as category_name', 'c.alias as category_alias',
+        'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+        'acc2.full_name as bidder_name')
+        .from('product as p')
+        .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+        .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+        .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+        .leftJoin('type as t', 't.type_id', 'p.type_id')
+        .leftJoin('category as c', 'c.category_id', 'p.category_id')
+        .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
+        .where('p.status', '1')
+    }
   },
   findById(product_id) {
     return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
@@ -26,13 +41,91 @@ module.exports = {
       .leftJoin('category as c', 'c.category_id', 'p.category_id')
       .where('p.status', '1').andWhere('p.product_id', product_id)
   },
-  getInfoAuctioneer(product_id){
+  findImageByProductId(product_id) {
+    return db.select('pi.image')
+      .from('product_image as pi')
+      .where('pi.status', '1').andWhere('pi.product_id', product_id)
+  },
+  findRelationCategory(category_id, product_id) {
+    return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+      'c.name as category_name', 'c.alias as category_alias',
+      'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+      'acc2.full_name as bidder_name')
+      .from('product as p')
+      .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+      .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+      .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+      .leftJoin('type as t', 't.type_id', 'p.type_id')
+      .leftJoin('category as c', 'c.category_id', 'p.category_id')
+      .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
+      .where('p.status', '1').whereNotIn('p.product_id', [product_id])
+      .andWhere('p.category_id', category_id)
+      .limit(5)
+
+  },
+  findAllByType_Id(type_id, condition_end_day = false) {
+    if (condition_end_day) {
+      return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+        'c.name as category_name', 'c.alias as category_alias',
+        'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+        'acc2.full_name as bidder_name')
+        .from('product as p')
+        .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+        .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+        .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+        .leftJoin('type as t', 't.type_id', 'p.type_id')
+        .leftJoin('category as c', 'c.category_id', 'p.category_id')
+        .where('p.status', '1').andWhere('p.type_id', type_id)
+    } else {
+      return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+        'c.name as category_name', 'c.alias as category_alias',
+        'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+        'acc2.full_name as bidder_name')
+        .from('product as p')
+        .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+        .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+        .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+        .leftJoin('type as t', 't.type_id', 'p.type_id')
+        .leftJoin('category as c', 'c.category_id', 'p.category_id')
+        .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
+        .where('p.status', '1').andWhere('p.type_id', type_id)
+    }
+  },
+  findAllByCategory_Id(category_id, condition_end_day=false) {
+    if (condition_end_day) {
+      return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+        'c.name as category_name', 'c.alias as category_alias',
+        'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+        'acc2.full_name as bidder_name')
+        .from('product as p')
+        .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+        .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+        .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+        .leftJoin('type as t', 't.type_id', 'p.type_id')
+        .leftJoin('category as c', 'c.category_id', 'p.category_id')
+        .where('p.status', '1').andWhere('p.category_id', category_id)
+    } else {
+      return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
+        'c.name as category_name', 'c.alias as category_alias',
+        'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+        'acc2.full_name as bidder_name')
+        .from('product as p')
+        .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+        .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+        .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+        .leftJoin('type as t', 't.type_id', 'p.type_id')
+        .leftJoin('category as c', 'c.category_id', 'p.category_id')
+        .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
+        .where('p.status', '1').andWhere('p.category_id', category_id)
+    }
+  },
+  getInfoAuctioneer(product_id) {
     return db.select('ad.*', 'ra.account_id', 'acc.full_name', 'ra.reason')
-    .from('auction as a')
-    .rightJoin('auction_detail as ad', 'a.auction_id', 'ad.auction_id')
-    .leftJoin('reject_auction as ra', 'ra.auction_detail_id', 'ad.auction_detail_id')
-    .leftJoin('account as acc', 'ra.account_id', 'acc.account_id')
-    .where('a.product_id', product_id).andWhere('a.status', 1)
+      .from('auction as a')
+      .rightJoin('auction_detail as ad', 'a.auction_id', 'ad.auction_id')
+      .leftJoin('reject_auction as ra', 'ra.auction_detail_id', 'ad.auction_detail_id')
+      .leftJoin('account as acc', 'ra.account_id', 'acc.account_id')
+      .where('a.product_id', product_id).andWhere('a.status', 1)
 
   },
   top_5_time_run_out() {
@@ -51,37 +144,37 @@ module.exports = {
       .orderBy('p.end_day', 'asc')
       .limit(5)
   },
-  top_5_highest_cost(){
+  top_5_highest_cost() {
     return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
-    'c.name as category_name', 'c.alias as category_alias',
-    'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
-    'acc2.full_name as bidder_name')
-    .from('product as p')
-    .leftJoin('auction as a', 'a.product_id', 'p.product_id')
-    .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
-    .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
-    .leftJoin('type as t', 't.type_id', 'p.type_id')
-    .leftJoin('category as c', 'c.category_id', 'p.category_id')
-    .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
-    .where('p.status', 1)
-    .orderBy('p.start_cost ', 'desc')
-    .limit(5)
+      'c.name as category_name', 'c.alias as category_alias',
+      'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+      'acc2.full_name as bidder_name')
+      .from('product as p')
+      .leftJoin('auction as a', 'a.product_id', 'p.product_id')
+      .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+      .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+      .leftJoin('type as t', 't.type_id', 'p.type_id')
+      .leftJoin('category as c', 'c.category_id', 'p.category_id')
+      .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
+      .where('p.status', 1)
+      .orderBy('p.start_cost ', 'desc')
+      .limit(5)
   },
-  top_5_highest_auction(){
+  top_5_highest_auction() {
     return db.select('p.*', 'a.count_auction', 't.name as type_name', 't.alias as type_alias',
-    'c.name as category_name', 'c.alias as category_alias',
-    'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
-    'acc2.full_name as bidder_name')
-    .from('auction as a')
-    .leftJoin('product as p', 'p.product_id', 'a.product_id')
-    .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
-    .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
-    .leftJoin('type as t', 't.type_id', 'p.type_id')
-    .leftJoin('category as c', 'c.category_id', 'p.category_id')
-    .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
-    .where('p.status', 1)
-    .orderBy('count_auction', 'desc')
-    .limit(5)
+      'c.name as category_name', 'c.alias as category_alias',
+      'a.auction_id as auction_id', 'a.bidder_id', 'acc1.full_name as seller_name',
+      'acc2.full_name as bidder_name')
+      .from('auction as a')
+      .leftJoin('product as p', 'p.product_id', 'a.product_id')
+      .leftJoin('account as acc1', 'acc1.account_id', 'p.seller_id')
+      .leftJoin('account as acc2', 'acc2.account_id', 'a.bidder_id')
+      .leftJoin('type as t', 't.type_id', 'p.type_id')
+      .leftJoin('category as c', 'c.category_id', 'p.category_id')
+      .whereRaw('TIMEDIFF(p.end_day, now()) > ?', 0)
+      .where('p.status', 1)
+      .orderBy('count_auction', 'desc')
+      .limit(5)
   }
 
 
--- routes/product.route.js
+++ routes/product.route.js
@@ -5,23 +5,52 @@ const productModel = require('../models/product.model');
 const schema = require('../schemas/product.schema');
 const router = express.Router();
 router.get('/', async (req, res) => {
-  const rs = await productModel.findAll();
+  const condition_end_day = req.query.condition_end_day || false;
+  const rs = await productModel.findAll(condition_end_day);
   res.status(200).json(rs);
 });
 
-router.get('/:id', async (req, res) => {
-  const product_id = req.params.id || 0;
+router.get('info/:id', async (req, res) => {
+  const product_id = parseInt(req.params.id) || 0;
   const infoProduct = await productModel.findById(product_id);
   const infoAuctioneers = await productModel.getInfoAuctioneer(product_id);
-  if(!infoProduct){
-    return res.status(400).json({message: 'Không tìm thấy thông tin sản phẩm'});
+  if (!infoProduct) {
+    return res.status(400).json({ message: 'Không tìm thấy thông tin sản phẩm' });
   }
-  //const rs = await productModel.findAll();
+  //get full images
+  const p_images = await productModel.findImageByProductId(product_id);
+  let image = [infoProduct[0].image];
+  for (let i = 0; i < p_images.size; i++) {
+    console.log(p_images[i]);
+  }
+  for (let item of p_images) {
+    image.push(item.image);
+  }
+  //relation product
+  const relation_product = await productModel.findRelationCategory(infoProduct[0].category_id, product_id);
+  infoProduct[0].image = image;
   res.status(200).json({
     infoProduct: infoProduct[0],
-    infoAuctioneers: infoAuctioneers
+    infoAuctioneers: infoAuctioneers,
+    relation_product: relation_product
   });
 });
+
+router.get('/type/:id', async (req, res)=>{
+  const condition_end_day = req.query.condition_end_day || false;
+  const type_id = parseInt(req.params.id) || 0;
+  const info_types = await productModel.findAllByType_Id(type_id, condition_end_day);
+  res.status(200).json({info_types});
+
+})
+
+router.get('/category/:id', async (req, res)=>{
+  const condition_end_day = req.query.condition_end_day || false;
+  const category_id = parseInt(req.params.id) || 0;
+  const info_types = await productModel.findAllByCategory_Id(category_id, condition_end_day);
+  res.status(200).json({info_types});
+
+})
 
 router.get('/home/top-5-time-run-out', async (req, res) => {
   const rs = await productModel.top_5_time_run_out();

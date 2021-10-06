5203c35683e9adf8dc1e608723c58a4588437555
HoangDuy1999 <tranhoangduy.911@gmail.com>
2021-10-06 10:05:03 +0700

    fix mail-server

--- templates/format_email.template.js
+++ templates/format_email.template.js
@@ -7,7 +7,7 @@ module.exports = {
       text: 'Chạo bạn',
       html: `<p>Chào Bạn <b>${username}</b>,</p>
             <p>Để xác nhận bạn xác nhận đăng ký tài khoản tại website đấu giá <b>S_auction</b> </p>
-            <p>MãOTP: <b>${otp}</b></p>
+            <p>Mã OTP: <b>${otp}</b></p>
             <div style="color: red;  font-style: italic;">Vui lòng không cung cấp mã OTP này cho ai khác. Xin cảm ơn! </div>
             <div>Chào bạn</div>
             `
